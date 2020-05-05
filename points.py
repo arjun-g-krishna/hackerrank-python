c155eea262bae0463461befe76dc505b5ef02d45
arjun <arjunradhakrishnan52@gmail.com>
2020-05-05 07:56:21 +0530

    calcualates the points during the course

---
+++ points.py
@@ -0,0 +1,7 @@
+skill_completed="Python Syntax"
+exercises_completed=13
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise=5
+point_total=100
+point_total+=(exercises_completed*points_per_exercise)
+print("I got "+str(point_total)+" points!")
