91a363e1a80e1367ffb4adb96e65cea330541bd3
Arjun G Krishna <you@example.com>
2020-05-05 19:49:48 +0530

    demonstrates the usage of dictionaries

---
+++ dict.py
@@ -0,0 +1,5 @@
+#Here I have created a dictionary of strings
+my_dict={"name":"Arjun","place":"Kerala","age":18}
+#Printing the output by accessing the dictionary contents
+print("Hello! my name is "+my_dict["name"]+"."+"I am "+str(my_dict["age"])+" years old.")
+print("I am from "+my_dict["place"]+".")
